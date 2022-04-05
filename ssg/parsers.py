265f5330b58470c72db1fc71f8d660ac2daf4d01
Benjamin Chin <benjamin.d.chin@gmail.com>
2022-04-05 17:44:02 -0400

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,21 +8,21 @@ class Parser():
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(path: Path, source: Path, dest: Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(path):
+    def read(self, path):
         with open(path, 'r') as file:
             return file
         
-    def write(path, dest, content, ext='.html'):
+    def write(self, path, dest, content, ext='.html'):
         full_path = dest / path.with_suffix(ext).name
 
-        with open(path, 'w') as file:
+        with open(full_path, 'w') as file:
             file.write(content)
 
     def copy(self, path, source, dest):
-        self.copy2(path, dest / source)
+        shutil.copy2(path, dest / source)
 
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
